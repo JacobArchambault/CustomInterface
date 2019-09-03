684e87f9fd7b50185da7ee34a32a3e4e6ed2ad66
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-03 12:05:48 -0400

    Add Ipointy call on circle with try/catch block.

--- CustomInterface/Program.cs
+++ CustomInterface/Program.cs
@@ -10,6 +10,25 @@ namespace CustomInterface
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("***** Fun with Interfaces *****\n");
+            /// Call Points property defined by IPointy.
+            Hexagon hex = new Hexagon();
+            Console.WriteLine("Points: {0}", hex.Points);
+
+            // Catch a possible InvalidCastException
+            Circle c = new Circle("Lisa");
+            IPointy itfPt = null;
+            try
+            {
+                itfPt = (IPointy)c;
+                Console.WriteLine(itfPt.Points);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
         }
     }
 }
